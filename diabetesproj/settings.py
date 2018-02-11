0a1331fcc546dd95ae828ef7590f22ad511ecb87
Luciekimotho <luciekimotho@gmail.com>
2018-02-11 23:53:25 +0300

    Removed previous migrations

--- diabetesproj/settings.py
+++ diabetesproj/settings.py
@@ -147,6 +147,12 @@ REST_FRAMEWORK = {
    # 'DEFAULT_PERMISSION_CLASSES': (
    #      'rest_framework.permissions.IsAdminUser'
    # ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
 
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
